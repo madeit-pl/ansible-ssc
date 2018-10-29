2807b5fe7e71407ddb47b0f502275283d10723a4
madeIT <biuro@madeit.pl>
2018-10-29 16:31:48 +0100

    sh script

--- prepareAnsibleHost.sh
+++ prepareAnsibleHost.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install gcc python-dev libkrb5-dev
-sudo apt-get install python-pip
+sudo apt-get -y install gcc python-dev libkrb5-dev
+sudo apt-get -y install python-pip
 sudo pip install -upgrade pip
 sudo pip install -upgrade virtualenv
 pip install pywinrm[kerberos]
-sudo apt-get install krb5-user
+sudo apt-get -y install krb5-user
 sudo pip install kerberos
 sudo pip install ansible
