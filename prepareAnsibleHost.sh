18fa25d103de03c8e5da8d926fd0d592b45a253a
madeIT <biuro@madeit.pl>
2018-10-30 20:45:18 +0100

    prepare ansible v0.5

--- prepareAnsibleHost.sh
+++ prepareAnsibleHost.sh
@@ -19,10 +19,12 @@ cd awx
 cd installer
 #modify installer file for alternative DNS
 
+echo 'awx_alternate_dns_servers="172.25.192.22, 172.24.69.45"' >> inventory
+
 #sudo cp -r ~/.local/lib/python2.7/site-packages/backports/ssl_match_hostname/ /usr/lib/python2.7/dist-packages/backports
 sudo cp -r /usr/local/lib/python2.7/dist-packages/backports/ssl_match_hostname/ /usr/lib/python2.7/dist-packages/backports
 
 
 ansible-playbook -i inventory install.yml
 docker exec -it awx_task bash -c 'curl -L https://raw.githubusercontent.com/madeit-pl/ansible-ssc/master/installKerberosInsideDocker.sh | bash'
-docker exec -it awx_task bash -c 'curl -L https://raw.githubusercontent.com/madeit-pl/ansible-ssc/master/krb5.conf >> /etc/krb5.conf'
+docker exec -it awx_task bash -c 'curl -L https://raw.githubusercontent.com/madeit-pl/ansible-ssc/master/krb5.conf > /etc/krb5.conf'
