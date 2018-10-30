9d4c0bad77146f9cc78c8472b2bdd8aa62a3df37
madeIT <biuro@madeit.pl>
2018-10-30 12:56:00 +0100

    kerberos_extend

--- installKerberosInsideDocker.sh
+++ installKerberosInsideDocker.sh
@@ -1,2 +1,2 @@
-sudo apt-get -y install krb5-workstation, krb5-devel, krb5-libs, gcc, python-devel
-sudo pip install kerberos
+yum -y install krb5-workstation, krb5-devel, krb5-libs, gcc, python-devel
+pip install kerberos
