14c18f6d16c729d590c5621159e2bb6e5f9d5092
madeit-pl <43850632+madeit-pl@users.noreply.github.com>
2018-12-05 13:05:43 +0100

    Update 01_prepareAnsibleHost.sh

--- 01_prepareAnsibleHost.sh
+++ 01_prepareAnsibleHost.sh
@@ -21,6 +21,7 @@ cd installer
 
 sed -i 's/host_port=80/host_port=127.0.0.1:8000/g' inventory
 sed -i 's/#awx_alternate_dns_servers="10.1.2.3,10.2.3.4"/awx_alternate_dns_servers="172.25.192.22,172.24.69.45"/g' inventory
+sed -i 's/tmp\/pgdocker/home\/awx\/pgdocker/g' inventory
 
 sudo cp -r /usr/local/lib/python2.7/dist-packages/backports/ssl_match_hostname/ /usr/lib/python2.7/dist-packages/backports
 
